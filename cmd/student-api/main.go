1721611e4305f5987e2f481c6a8ed736fe553786
Bahlul19 <bahlul.tausif@gmail.com>
2025-08-06 22:34:55 -0400

    configuartion completed, server run successfully on the terminal and browser

--- cmd/student-api/main.go
+++ cmd/student-api/main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"go_project/internal/config"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Welcome to the Student API!")
+	//load config
+	cfg := config.MustLoad()
+	//database connection
+
+	//setup router
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to Student API!"))
+	})
+
+	//setup server
+	server := http.Server{
+		Addr:    cfg.Addr,
+		Handler: router,
+	}
+
+	fmt.Printf("Server is running %s", cfg.HTTPServer.Addr)
+
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal("Failed to start the server")
+	}
+	// fmt.Println("Server is running on")
 }
